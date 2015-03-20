8c4a8758fd661b37030643dfc9f7dfecf5507b3a
bboakye <brittanyb934@gmail.com>
2015-03-20 08:52:11 -0400

    Update application.js

--- ShoppingList/application.js
+++ ShoppingList/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-$('#button').on('click',function()){
+$('#button').on('click',function()
+{
   var item=$('#placeholder').val();
 $("#my_shopinglist").append(item)
-}
+})
   
 })
